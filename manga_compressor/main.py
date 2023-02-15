b18c3128eb012301b135bb107d9979091f9dba38
winterreisender <winterreisender@foxmail.com>
2023-02-15 14:12:17 +0800

    init

--- manga_compressor/main.py
+++ manga_compressor/main.py
@@ -10,34 +10,69 @@ def compress_folder(input_dir,output_dir):
     
 
 def main(page: ft.Page):
-    page.title = "Flet counter example"
-    page.vertical_alignment = ft.MainAxisAlignment.CENTER
+    page.title = "Manga"
+    page.theme_mode = ft.ThemeMode.LIGHT
+    page.padding = 50
+    page.update()
 
-    txt_number = ft.TextField(value="0", text_align=ft.TextAlign.RIGHT, width=100)
+    # State
+    pictures = []
 
-    def minus_click(e):
-        txt_number.value = str(int(txt_number.value) - 1)
+    # Images
+    images = ft.Row(expand=1, wrap=True, scroll="always")
+    page.add(images)
+
+    # File Picker
+    def update_pictures(v):
+        pictures = v
+        for picture in pictures:
+            images.controls.append(
+                ft.Column([
+                ft.Image(
+                    src=picture.path,
+                    width=200,
+                    height=150,
+                    fit=ft.ImageFit.NONE,
+                    repeat=ft.ImageRepeat.NO_REPEAT,
+                    border_radius=ft.border_radius.all(10),
+                ),
+                ft.Text(f'{round(picture.size/1024,1)} KB'),
+                ft.Text(picture.name)
+                ])
+            )
         page.update()
+    file_picker = ft.FilePicker(on_result=lambda e: update_pictures(e.files))
+    page.overlay.append(ft.FilePicker(ref=file_picker))
+    page.add(file_picker)
 
-    def plus_click(e):
-        txt_number.value = str(int(txt_number.value) + 1)
-        page.update()
-
+    # Pick File Button
+    def on_pick_clicked(e):
+        file_picker.pick_files(allow_multiple=True)
     page.add(
-        ft.Row(
-            [
-                ft.IconButton(ft.icons.REMOVE, on_click=minus_click),
-                txt_number,
-                ft.IconButton(ft.icons.ADD, on_click=plus_click),
-            ],
-            alignment=ft.MainAxisAlignment.CENTER,
-        )
+        ft.ElevatedButton("添加图片",on_click=on_pick_clicked)
     )
 
+    page.update()
+    return
+
+    images = ft.Row(expand=1, wrap=False, scroll="always")
+
+    for picture in []:
+        images.controls.append(
+            ft.Image(
+                src=picture.path,
+                width=200,
+                height=150,
+                fit=ft.ImageFit.NONE,
+                repeat=ft.ImageRepeat.NO_REPEAT,
+                border_radius=ft.border_radius.all(10),
+            )
+        )
+    
+    page.add(images)
+    page.update()
 
 
 if __name__ == '__main__':
     print("hello")
     ft.app(target=main)
-    compress_folder('.','.')
-    
